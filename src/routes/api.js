08945911a55f031544658b798b7da5ced2d09710
Lee Mulvey <lmulvey@me.com>
2019-11-09 14:00:16 -0700

    Rename helper method.

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,12 @@
 import Express from 'express';
-import { calculateVehicleEmissions } from '../lib/carbonHelpers';
+import { calculateEmissions } from '../lib/carbonHelpers';
 const router = Express.Router();
 
 router.post('/footprint', (req, res) => {
   // This takes a distance for now but can be changed out.
   const distance = req.body.distance;
   const parsedDistance = parseFloat(distance);
-  const carbonEmissions = calculateVehicleEmissions(parsedDistance);
+  const carbonEmissions = calculateEmissions(parsedDistance);
   res.json({ carbonEmissions });
 });
 
