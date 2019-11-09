10abfbc78adfc5691e76533e60b80412d3bb6c45
Lee Mulvey <lmulvey@me.com>
2019-11-09 16:39:56 -0700

    Add Polyline and a base route for Heroku

--- src/server.js
+++ src/server.js
@@ -10,6 +10,11 @@ const app = Express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
+app.get("/", (req, res) => {
+  res.send(
+    "Please use the API at /api/footprint to grab carbon emissions data"
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
