371631aa2f9384e66c2a91ca32475a934367ef4f
Lee Mulvey <lmulvey@me.com>
2019-11-09 15:00:43 -0700

    fixes

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,8 @@
-import Express from 'express';
-import { calculateEmissions } from '../lib/carbonHelpers';
+import Express from "express";
+import { calculateEmissions } from "../lib/carbonHelpers";
 const router = Express.Router();
 
-router.post('/footprint', (req, res) => {
+router.post("/footprint", (req, res) => {
   // This takes a distance for now but can be changed out.
   const distance = req.body.distance;
   const parsedDistance = parseFloat(distance);
