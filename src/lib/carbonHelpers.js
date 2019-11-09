7a8bb1cde9eb6790b857292d20e044caecdaad79
Lee Mulvey <lmulvey@me.com>
2019-11-09 14:15:35 -0700

    ESLint and Prettier

--- src/lib/carbonHelpers.js
+++ src/lib/carbonHelpers.js
@@ -1,25 +1,11 @@
 import { TRANSPORT_TYPES, validateTransportType } from "./transportHelpers";
 
 /**
- * Method for calculating carbon emissions for a single vehicle trip.
- * @param {Interger} distance  - distance of travel in kilometers
- */
-export function calculateEmissions(distance, transportType) {
-  if (typeof distance !== 'number') {
-    console.log('Error occurred');
-    throw new Error('Invalid distance provided. Distance should be a number in kilometers.');
-  }
-
-  const EMISSION_FACTOR_PER_KM = _resolveEmissionFactor(transportType);
-  return (distance * EMISSION_FACTOR_PER_KM).toFixed(4);
-}
-
-/**
  * Helper method to return the proper emissions factor for each transportation type.
  * Additionally validates the input to confirm that we support the passed in transportation type.
  * @param {String} transportType - type of transport to calculate emissions for
  */
-function _resolveEmissionFactor(transportType) {
+function resolveEmissionFactor(transportType) {
   const emissionFactorMap = {
     /**
      * This number is based off of IEA GFEI report for Fuel Economy.
@@ -34,9 +20,25 @@ function _resolveEmissionFactor(transportType) {
 
     // TODO: Validate that these are ACTUALLY zero.
     [TRANSPORT_TYPES.BIKE]: 0,
-    [TRANSPORT_TYPES.WALK]: 0,
-  }
+    [TRANSPORT_TYPES.WALK]: 0
+  };
 
   const validTransportType = validateTransportType(transportType);
   return emissionFactorMap[validTransportType];
+}
+
+/**
+ * Method for calculating carbon emissions for a single vehicle trip.
+ * @param {Interger} distance  - distance of travel in kilometers
+ */
+export function calculateEmissions(distance, transportType) {
+  if (typeof distance !== "number") {
+    console.log("Error occurred");
+    throw new Error(
+      "Invalid distance provided. Distance should be a number in kilometers."
+    );
+  }
+
+  const EMISSION_FACTOR_PER_KM = resolveEmissionFactor(transportType);
+  return (distance * EMISSION_FACTOR_PER_KM).toFixed(4);
 }
--- src/lib/transportHelpers.js
+++ src/lib/transportHelpers.js
@@ -1,24 +1,28 @@
 import { titleCase } from "./genericHelpers";
 
 export const TRANSPORT_TYPES = {
-  VEHICLE: 'VEHICLE',
-  PUBLIC: 'PUBLIC',
-  BIKE: 'BIKE',
-  WALK: 'WALK'
+  VEHICLE: "VEHICLE",
+  PUBLIC: "PUBLIC",
+  BIKE: "BIKE",
+  WALK: "WALK"
 };
 
 export function validateTransportType(inputType) {
-  if (typeof inputType !== 'string') {
-    throw new Error('Vehicle Type should be a string.');
+  if (typeof inputType !== "string") {
+    throw new Error("Vehicle Type should be a string.");
   }
 
   const lowerCaseType = inputType.toLowerCase();
   const typeCandidate = TRANSPORT_TYPES[lowerCaseType];
-  
+
   if (!typeCandidate) {
-    const prettyTypes = Object.keys(TRANSPORT_TYPES).map((key) => titleCase(key)).join(', ');
-    throw new Error(`Invalid transport type. Transport should be one of the following: ${prettyTypes}`);
+    const prettyTypes = Object.keys(TRANSPORT_TYPES)
+      .map(key => titleCase(key))
+      .join(", ");
+    throw new Error(
+      `Invalid transport type. Transport should be one of the following: ${prettyTypes}`
+    );
   }
 
-  return typeCandidate
+  return typeCandidate;
 }
