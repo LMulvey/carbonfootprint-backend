7bd1522eba546eace0108bd8e896e96f1fcdfefb
Lee Mulvey <lmulvey@me.com>
2019-11-09 16:41:58 -0700

    Change route to be a queryString/get not a post

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,9 +2,9 @@ import Express from "express";
 import { calculateEmissions } from "../lib/carbonHelpers";
 const router = Express.Router();
 
-router.post("/footprint", (req, res) => {
+router.get("/footprint", (req, res) => {
   // This takes a distance for now but can be changed out.
-  const distance = req.body.distance;
+  const distance = req.query.distance;
   const parsedDistance = parseFloat(distance);
   const carbonEmissions = calculateEmissions(parsedDistance);
   res.json({ carbonEmissions });
