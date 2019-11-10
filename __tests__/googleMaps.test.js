30f3853f9372637abe777661ad73454a04bc864c
Lee Mulvey <lmulvey@me.com>
2019-11-09 22:00:11 -0700

    Fix tests.

--- __tests__/googleMaps.test.js
+++ __tests__/googleMaps.test.js
@@ -4,7 +4,7 @@ describe("GoogleMaps", () => {
   test("totalEmissionsForRoute | Returns totalEmissions for a route in g/CO2", async () => {
     const origin = "place_id:ChIJtUiovwJwcVMRBCqAUqr-Ed4";
     const destination = "place_id:ChIJN2IIz_xvcVMRXOZpdWodzrc";
-    const expectedEmissions = 485.748;
+    const expectedEmissions = 0.4857;
     const totalEmissions = await getRoutesForAllTypes(origin, destination);
     expect(totalEmissions.DRIVING.totalEmissionsForRoute).toEqual(
       expectedEmissions
